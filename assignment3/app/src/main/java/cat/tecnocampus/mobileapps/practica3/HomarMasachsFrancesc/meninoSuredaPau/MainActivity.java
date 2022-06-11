5da95e807b4d45ac49c3940cf648d83e3fc56ce4
FhomarTCM <fhomar@edu.tecnocampus.cat>
2022-06-12 00:13:04 +0200

    Creation project

---
+++ assignment3/app/src/main/java/cat/tecnocampus/mobileapps/practica3/HomarMasachsFrancesc/meninoSuredaPau/MainActivity.java
@@ -0,0 +1,14 @@
+package cat.tecnocampus.mobileapps.practica3.HomarMasachsFrancesc.meninoSuredaPau;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
