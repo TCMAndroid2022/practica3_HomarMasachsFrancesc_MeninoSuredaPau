4b5e142c542d7d156f77bcad76f06f4a52c37ce3
FhomarTCM <fhomar@edu.tecnocampus.cat>
2022-06-12 00:38:29 +0200

    No se que hacer para que vaya esto

--- assignment3/app/src/main/java/cat/tecnocampus/mobileapps/practica3/HomarMasachsFrancesc/meninoSuredaPau/MainActivity.java
+++ assignment3/app/src/main/java/cat/tecnocampus/mobileapps/practica3/HomarMasachsFrancesc/meninoSuredaPau/MainActivity.java
@@ -3,12 +3,18 @@ package cat.tecnocampus.mobileapps.practica3.HomarMasachsFrancesc.meninoSuredaPa
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView ivIMG;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ivIMG = findViewById(R.id.ivImg);
+        Glide.with(this).load("https://imgur.com/aUBispH").into(ivIMG);
     }
 }
